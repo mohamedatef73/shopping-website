801aef02cc562bdf33d8ed70a248c6982af63937
mohamed atef <59102121+mohamedatef73@users.noreply.github.com>
2020-10-30 18:08:40 +0200

    create firebase configration file

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+        apiKey: "AIzaSyDH_CDHiJag1utU4dnjOmszK_B3iM7GWNg",
+        authDomain: "ecommerce-website-32484.firebaseapp.com",
+        databaseURL: "https://ecommerce-website-32484.firebaseio.com",
+        projectId: "ecommerce-website-32484",
+        storageBucket: "ecommerce-website-32484.appspot.com",
+        messagingSenderId: "324540378554",
+        appId: "1:324540378554:web:355f2809d15d5d00638183",
+        measurementId: "G-CEDX2SRJ6W"
+
+}
